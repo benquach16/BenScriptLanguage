bc1498dce8770d9e4a80efef12e9bd42ad5beb4e
thesultanster <khansultan024@gmail.com>
2014-02-28 19:48:05 -0800

    getMatchingParens

---
+++ getMatchingParens.cpp
@@ -0,0 +1,15 @@
+void Lexer::GetMatchingParens(unsigned leftParens, vector<string> &tokens)
+{
+	int leftParensCount = 0;
+	for (int i = 0; i < token.size(); i++)
+	{
+		if (tokens[0] == ‘(’)  // If a left parenthesis is found
+			leftParensCount++; // increment 
+		else if (tokens[0] == ‘)’) // otherwise if a right parenthesis is found
+		if (leftParensCount == 0) // check if this should be the last right parens
+			return i;             // and return the index of it
+		else
+			leftParensCount--;    // otherwise it's not last right parens and decrement
+	}
+	cerr << "ERROR at line: " << currentLine << " You are missing at least one right parenthesis" << endl;
+}
