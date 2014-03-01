ffa16c8398b00551ea20128dd80a6223afca1272
benquach16 <benquach16@yahoo.com>
2014-02-28 22:50:56 -0800

    doline file

--- doline.cpp
+++ doline.cpp
@@ -1,30 +1,68 @@
 void doLine(const vector<string> &tokens)
 {
-	
-	for(unsigned i = 0; i < tokens.size(); i++)
+	if(tokens.size() > 1)
 	{
-		if(tokens[i] == "=")
+		for(unsigned i = 0; i < tokens.size(); i++)
 		{
-			//found the assignment operator - most important
-			//split into to two vectors
-			vector<string> leftTokens;
-			vector<string> rightTokens;
-			//allocate to these vectors
-			for(unsigned n = 0; n < i; n++)
+			if(tokens[i] == "=" || tokens[i] == "+=" || tokens[i] == "-=" ||
+				 tokens[i] == "*=" || tokens[i] == "/=" || tokens[i] == "%=")
 			{
-				leftTokens.push_back(tokens[n]);
-			}
-			for(unsigned n = tokens.size(); n > i; n--)
-			{
-				rightTokens.push_back(tokens[n]);
-			}
+				//found the assignment operator - most important
+				//split into to two vectors
+				vector<string> leftTokens;
+				vector<string> rightTokens;
+				//allocate to these vectors
+				for(unsigned n = 0; n < i; n++)
+				{
+					leftTokens.push_back(tokens[n]);
+				}
+				for(unsigned n = tokens.size(); n > i; n--)
+				{
+					rightTokens.push_back(tokens[n]);
+				}
 
-			doLine(leftTokens);
-			doLine(rightTokens);
-			//now we can assign it
+				//doLine(leftTokens);
+				//doLine(rightTokens);
+				operatorSelect(leftTokens, rightTokens, tokens[i]);
+				
+			}
 		}
-
+		//we didnt find the assignment so we go on to the other sijdes
+		for(unsigned i = 0; i < tokens.size(); i++)
+		{
+			//or operator
+			if(tokens[i] == "||")
+			{
+			}
+		}
+		//no or operator? lets go through it again
+		for(unsigned i = 0; i < tokens.size(); i++)
+		{
+			if(tokens[i] == "&&")
+			{
+			}
+		}
+		//handle comparison operators
+		for(unsigned i = 0; i < tokens.size(); i++)
+		{
+			if(tokens[i] == "==" || tokens[i] == "!=")
+			{
+			
+			}
+		}
+		//handle more comparision operators
+		for(unsigned i = 0; i < tokens.size(); i++)
+		{
+			if(tokens[i] == "<=" || tokens[i] == ">=" || tokens[i] == ">"
+				 tokens[i] == "<")
+			{
+			
+			}
+		}
+		//handle the arthimetic operators
+		for(unsigned i = 0; i < tokens.size(); i++)
+		{
 		
+		}
 	}
-
 }
--- lexer.cpp
+++ lexer.cpp
@@ -15,7 +15,7 @@ void Lexer::ReadFile(char *filePath)
 {
   // checks if file exists
   ifstream file;
-  file.open(filePath);
+	file.open(filePath);
   
   if(!file.is_open())
 	{
@@ -167,75 +167,75 @@ vector<string> Lexer::TokenizeLine(const string &str)
 
   // traverse line end if get to end or hit comment
   while(i != str.size() && str[i] != '#')
-    {
-      //if hit function operators push them back
-      if(str[i] == '(' || str[i] == ')' || 
-	 str[i] == ',' || str[i] == '?' || str[i] == ':')
 	{
-	  if(curToken.size() > 0)
+		//if hit function operators push them back
+		if(str[i] == '(' || str[i] == ')' || 
+			 str[i] == ',' || str[i] == '?' || str[i] == ':')
+		{
+			if(curToken.size() > 0)
 	    {
 	      tokens.push_back(curToken);
 	      curToken = "";
 	    }
 
-	  tokens.push_back(str.substr(i,1));
-	}
+			tokens.push_back(str.substr(i,1));
+		}
 
-      // if hit math operators push them back
-      else if(str[i] == '=' || str[i] == '+' || str[i] == '-' ||
-	      str[i] == '/' || str[i] == '*' || str[i] == '>' ||
-	      str[i] == '<')
-	{
-	  if(curToken.size() > 0)
+		// if hit math operators push them back
+		else if(str[i] == '=' || str[i] == '+' || str[i] == '-' ||
+						str[i] == '/' || str[i] == '*' || str[i] == '>' ||
+						str[i] == '<')
+		{
+			if(curToken.size() > 0)
 	    {
 	      tokens.push_back(curToken);
 	      curToken = "";
 	    }
 
-	  // if += ++ etc
-	  if(str.size() < i + 1 && (str[i+1] == '=') || (str[i+1] == '+') ||
-	     (str[i+1] == '-') && str[i] == str[i+1])
+			// if += ++ etc
+			if(str.size() < i + 1 && (str[i+1] == '=') || (str[i+1] == '+') ||
+				 (str[i+1] == '-') && str[i] == str[i+1])
 	    {
 	      tokens.push_back(str.substr(i,2));
 	      i++;
 	    }
 
-	  else
-	    tokens.push_back(str.substr(i,1));
-	}
+			else
+				tokens.push_back(str.substr(i,1));
+		}
 
-      // if or and
-      else if((str[i] == '|' || str[i] == '&')  && (str.size() > i+1 && str[i+1] == str[i]))
-	{
-	  if(curToken.size() > 0)
+		// if or and
+		else if((str[i] == '|' || str[i] == '&')  && (str.size() > i+1 && str[i+1] == str[i]))
+		{
+			if(curToken.size() > 0)
 	    {
 	      tokens.push_back(curToken);
 	      curToken = "";
 	    }
 
-	  tokens.push_back(str.substr(i,2));
-	  i++;
-	}
+			tokens.push_back(str.substr(i,2));
+			i++;
+		}
 
-      // if hit space reset curToken
-      else if(str[i] == ' ')
-	{
-	  if(curToken.size() > 0)
+		// if hit space reset curToken
+		else if(str[i] == ' ')
+		{
+			if(curToken.size() > 0)
 	    {
 	      tokens.push_back(curToken);
 	      curToken = "";
 	    }
-	}
+		}
       
-      // if nothing add it to curToken
-      else
-	curToken += str[i];
+		// if nothing add it to curToken
+		else
+			curToken += str[i];
 
-      i++;
-    }
+		i++;
+	}
 
   if(curToken.size() > 0)
     tokens.push_back(curToken);
 
-return tokens;
+	return tokens;
 }
