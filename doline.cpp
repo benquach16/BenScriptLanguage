d6f2411eb2373082dc072fef99e0155cf080e2f9
darawl <dylanjay9@gmail.com>
2014-03-01 02:21:05 -0800

    doline.cpp

--- doline.cpp
+++ doline.cpp
@@ -1,68 +1,209 @@
-void doLine(const vector<string> &tokens)
-{
-	if(tokens.size() > 1)
-	{
-		for(unsigned i = 0; i < tokens.size(); i++)
-		{
-			if(tokens[i] == "=" || tokens[i] == "+=" || tokens[i] == "-=" ||
-				 tokens[i] == "*=" || tokens[i] == "/=" || tokens[i] == "%=")
-			{
-				//found the assignment operator - most important
-				//split into to two vectors
-				vector<string> leftTokens;
-				vector<string> rightTokens;
-				//allocate to these vectors
-				for(unsigned n = 0; n < i; n++)
-				{
-					leftTokens.push_back(tokens[n]);
-				}
-				for(unsigned n = tokens.size(); n > i; n--)
-				{
-					rightTokens.push_back(tokens[n]);
-				}
+#include <iostream>
+#include <vector>
+#include "lexer.h"
 
-				//doLine(leftTokens);
-				//doLine(rightTokens);
-				operatorSelect(leftTokens, rightTokens, tokens[i]);
-				
-			}
-		}
-		//we didnt find the assignment so we go on to the other sijdes
-		for(unsigned i = 0; i < tokens.size(); i++)
-		{
-			//or operator
-			if(tokens[i] == "||")
-			{
-			}
-		}
-		//no or operator? lets go through it again
-		for(unsigned i = 0; i < tokens.size(); i++)
-		{
-			if(tokens[i] == "&&")
-			{
-			}
-		}
-		//handle comparison operators
-		for(unsigned i = 0; i < tokens.size(); i++)
-		{
-			if(tokens[i] == "==" || tokens[i] == "!=")
-			{
-			
-			}
-		}
-		//handle more comparision operators
-		for(unsigned i = 0; i < tokens.size(); i++)
-		{
-			if(tokens[i] == "<=" || tokens[i] == ">=" || tokens[i] == ">"
-				 tokens[i] == "<")
-			{
-			
-			}
-		}
-		//handle the arthimetic operators
-		for(unsigned i = 0; i < tokens.size(); i++)
-		{
-		
-		}
-	}
+using namespace std;
+
+// checks assignment operators
+int Lexer::findOperator1(vector <string> tokens)
+{
+  int parens = 0;
+  for(int i = 0; i < tokens.size(); i++)
+  {
+    if(tokens.at(i) == "(")
+      parens++;
+
+    else if(tokens.at(i) == ")")
+      parens--;
+
+    if(parens == 0 && tokens.at(i) == "+=" || tokens.at(i) == "?" ||
+       tokens.at(i) == "-=" || tokens.at(i) == "*=" || tokens.at(i) == "/=" ||
+       tokens.at(i) == "=" || tokens.at(i) == "%=")
+      return i;
+  }
+  return -1;
+}
+
+// checks or
+int Lexer::findOperator2(vector <string> tokens)
+{
+  int parens = 0;
+  for(int i = 0; i < tokens.size(); i++)
+  {
+    if(tokens.at(i) == "(")
+      parens++;
+
+    else if(tokens.at(i) == ")")
+      parens--;
+
+    if(parens == 0 && tokens.at(i) == "||")
+      return i;
+  }
+  return -1;
+}
+
+// checks and
+int Lexer::findOperator3(vector <string> tokens)
+{
+  int parens = 0;
+  for(int i = 0; i < tokens.size(); i++)
+  {
+    if(tokens.at(i) == "(")
+      parens++;
+
+    else if(tokens.at(i) == ")")
+      parens--;
+
+    if(parens == 0 && tokens.at(i) == "&&")
+      return i;
+  }
+  return -1;
+}
+
+// checks equality comparison
+int Lexer::findOperator4(vector <string> tokens)
+{
+  int parens = 0;
+  for(int i = 0; i < tokens.size(); i++)
+  {
+    if(tokens.at(i) == "(")
+      parens++;
+
+    else if(tokens.at(i) == ")")
+      parens--;
+
+    if(parens == 0 && tokens.at(i) == "==" || tokens.at(i) == "!=")
+      return i;
+  }
+  return -1;
+}
+
+// checks less than or greater than comparisons
+int Lexer::findOperator5(vector <string> tokens)
+{
+  int parens = 0;
+  for(int i = 0; i < tokens.size(); i++)
+  {
+    if(tokens.at(i) == "(")
+      parens++;
+
+    else if(tokens.at(i) == ")")
+      parens--;
+
+    if(parens == 0 && tokens.at(i) == "<=" || tokens.at(i) == ">=" ||
+      tokens.at(i) == "<" || tokens.at(i) == ">")
+      return i;
+  }
+  return -1;
+}
+
+// checks add and subtract
+int Lexer::findOperator6(vector <string> tokens)
+{
+  int parens = 0;
+  for(int i = 0; i < tokens.size(); i++)
+  {
+    if(tokens.at(i) == "(")
+      parens++;
+
+    else if(tokens.at(i) == ")")
+      parens--;
+
+    if(parens == 0 && tokens.at(i) == "+" || tokens.at(i) == "-")
+      return i;
+  }
+  return -1;
+}
+
+// checks multiply and divide
+int Lexer::findOperator7(vector <string> tokens)
+{
+  int parens = 0;
+  for(int i = 0; i < tokens.size(); i++)
+  {
+    if(tokens.at(i) == "(")
+      parens++;
+
+    else if(tokens.at(i) == ")")
+      parens--;
+
+    if(parens == 0 && tokens.at(i) == "*" || tokens.at(i) == "/")
+      return i;
+  }
+  return -1;
+}
+
+// selects operation and executes left and right
+Variable Lexer::operatorSelect(vector<string> left, vector<string> right, string op)
+{
+}
+
+// recursively separates tokens into operators
+Variable Lexer::split(int index, vector<string> tokens)
+{
+  vector<string> leftTokens;
+  vector<string> rightTokens;
+  //allocate to these vectors
+  for(unsigned n = 0; n < index; n++)
+  {
+    leftTokens.push_back(tokens[n]);
+  }
+  for(unsigned n = tokens.size(); n > index; n--)
+  {
+    rightTokens.push_back(tokens[n]);
+  }
+  
+  doLine(leftTokens);
+  doLine(rightTokens);
+  return operatorSelect(leftTokens, rightTokens, tokens[index]);
+}
+
+Variable Lexer::doLine(vector<string> &tokens)
+{
+  if(tokens.size() > 1)
+  {
+    // shave outer parens
+    if(tokens.at(0) == "(" && tokens.at(tokens.size()-1) == ")" )
+    {
+      tokens.pop_back();
+      tokens.erase(tokens.begin());
+    }
+    
+    int mid;
+    
+    // checks all assignment operators
+    mid = findOperator1(tokens);
+    if(mid != -1)
+      return split(mid, tokens);
+    
+    // checks or
+    mid = findOperator2(tokens);
+    if(mid != -1)
+      return split(mid, tokens);
+    
+    // checks and
+    mid = findOperator3(tokens);
+    if(mid != -1)
+      return split(mid, tokens);
+    
+    // checks equality comparison
+    mid = findOperator4(tokens);
+    if(mid != -1)
+      return split(mid, tokens);
+    
+    // checks less than or greater than comparisons
+    mid = findOperator5(tokens);
+    if(mid != -1)
+      return split(mid, tokens);
+    
+    // checks add or subtract
+    mid = findOperator6(tokens);
+    if(mid != -1)
+      return split(mid, tokens);
+    
+    // checks multiply and divide
+    mid = findOperator7(tokens);
+    if(mid != -1)
+      return split(mid, tokens);
+  }
 }
