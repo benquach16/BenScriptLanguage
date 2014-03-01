3d1d62961ad1ff0e79772b7177a10ed56756255d
thesultanster <khansultan024@gmail.com>
2014-02-28 22:07:58 -0800

    Adding my own test main

---
+++ testmain.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "lexer.h"
+
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+	for (unsigned i = 0; i < argc; i++)
+	{
+		Lexer interpreter;
+		//interpreter.FirstPass(argv[i]);
+	}
+	return 0;
+}
