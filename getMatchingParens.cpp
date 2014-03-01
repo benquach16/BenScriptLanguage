3c68e68bfd90ddaaac25b48a4fd13b7a680f590a
thesultanster <khansultan024@gmail.com>
2014-02-28 19:48:47 -0800

    getmatchingparens

--- getMatchingParens.cpp
+++ getMatchingParens.cpp
@@ -3,9 +3,9 @@ void Lexer::GetMatchingParens(unsigned leftParens, vector<string> &tokens)
 	int leftParensCount = 0;
 	for (int i = 0; i < token.size(); i++)
 	{
-		if (tokens[0] == ‘(’)  // If a left parenthesis is found
+		if (tokens[0] == '(')  // If a left parenthesis is found
 			leftParensCount++; // increment 
-		else if (tokens[0] == ‘)’) // otherwise if a right parenthesis is found
+		else if (tokens[0] == ')') // otherwise if a right parenthesis is found
 		if (leftParensCount == 0) // check if this should be the last right parens
 			return i;             // and return the index of it
 		else
