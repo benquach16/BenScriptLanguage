599bae6819299f0670edf9675626e81853fe36fa
darawl <dylanjay9@gmail.com>
2014-02-28 22:09:02 -0800

    tokenizer.cpp

--- tokenizer.cpp
+++ tokenizer.cpp
@@ -13,85 +13,85 @@ vector<string> TokenizeLine(const string &str)
 
   // traverse line end if get to end or hit comment
   while(i != str.size() && str[i] != '#')
+  {
+    //if hit function operators push them back
+    if(str[i] == '(' || str[i] == ')' || 
+       str[i] == ',' || str[i] == '?' || str[i] == ':')
     {
-      //if hit function operators push them back
-      if(str[i] == '(' || str[i] == ')' || 
-	 str[i] == ',' || str[i] == '?' || str[i] == ':')
-	{
-	  if(curToken.size() > 0)
-	    {
-	      tokens.push_back(curToken);
-	      curToken = "";
-	    }
+      if(curToken.size() > 0)
+      {
+	tokens.push_back(curToken);
+	curToken = "";
+      }
 
-	  tokens.push_back(str.substr(i,1));
-	}
+      tokens.push_back(str.substr(i,1));
+    }
 
-      // if hit math operators push them back
-      else if(str[i] == '=' || str[i] == '+' || str[i] == '-' ||
-	      str[i] == '/' || str[i] == '*' || str[i] == '>' ||
-	      str[i] == '<')
-	{
-	  if(curToken.size() > 0)
-	    {
-	      tokens.push_back(curToken);
-	      curToken = "";
-	    }
+    // if hit math operators push them back
+    else if(str[i] == '=' || str[i] == '+' || str[i] == '-' ||
+	    str[i] == '/' || str[i] == '*' || str[i] == '>' ||
+	    str[i] == '<')
+    {
+      if(curToken.size() > 0)
+      {
+	tokens.push_back(curToken);
+	curToken = "";
+      }
 
-	  // if += ++ etc
-	  if(str.size() < i + 1 && (str[i+1] == '=') || (str[i+1] == '+') ||
-	     (str[i+1] == '-') && str[i] == str[i+1])
-	    {
-	      tokens.push_back(str.substr(i,2));
-	      i++;
-	    }
+      // if += ++ etc
+      if(str.size() > i + 1 && (str[i+1] == '=') || (str[i+1] == '+') ||
+	 (str[i+1] == '-') && str[i] == str[i+1])
+      {
+	tokens.push_back(str.substr(i,2));
+	i++;
+      }
 
-	  else
-	    tokens.push_back(str.substr(i,1));
-	}
-
-      // if or and
-      else if((str[i] == '|' || str[i] == '&')  && (str.size() > i+1 && str[i+1] == str[i]))
-	{
-	  if(curToken.size() > 0)
-	    {
-	      tokens.push_back(curToken);
-	      curToken = "";
-	    }
-
-	  tokens.push_back(str.substr(i,2));
-	  i++;
-	}
-
-      // if hit space reset curToken
-      else if(str[i] == ' ')
-	{
-	  if(curToken.size() > 0)
-	    {
-	      tokens.push_back(curToken);
-	      curToken = "";
-	    }
-	}
-      
-      // if nothing add it to curToken
       else
-	curToken += str[i];
+	tokens.push_back(str.substr(i,1));
+    }
 
+    // if or and
+    else if((str[i] == '|' || str[i] == '&')  && (str.size() > i+1 && str[i+1] == str[i]))
+    {
+      if(curToken.size() > 0)
+      {
+	tokens.push_back(curToken);
+	curToken = "";
+      }
+
+      tokens.push_back(str.substr(i,2));
       i++;
     }
+
+    // if hit space reset curToken
+    else if(str[i] == ' ')
+    {
+      if(curToken.size() > 0)
+      {
+	tokens.push_back(curToken);
+	curToken = "";
+      }
+    }
+      
+    // if nothing add it to curToken
+    else
+      curToken += str[i];
+
+    i++;
+  }
 
   if(curToken.size() > 0)
     tokens.push_back(curToken);
 
-return tokens;
+  return tokens;
 }
 
 int main()
 {
   vector <string> tokens;
-  string line = "const string <bash> + doods = 4";
+  string line = "int a+=dry && int b == c + 4 || s || s";
   tokens = TokenizeLine(line);
   cout <<tokens.size() << endl;
   for(int i = 0; i < tokens.size(); i++)
-      cout << "'" << tokens.at(i) << "'" << endl;
+    cout << "'" << tokens.at(i) << "'" << endl;
 }
