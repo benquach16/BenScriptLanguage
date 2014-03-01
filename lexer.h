cd9b49da017d4dd3d68d332e87fca0416f182424
U-Ben-PC\Ben <Ben@Ben-PC.(none)>
2014-02-28 19:12:25 -0800

    lexer

--- lexer.h
+++ lexer.h
@@ -4,16 +4,48 @@
 
 using namespace std;
 
+struct Function
+{
+	string name;
+	unsigned fileLine;
+	vector<string> functionArguments;
+};
+
+enum VariableTypes
+{
+	int,
+	float,
+	bool,
+	string
+};
+
+struct Variable
+{
+	string name;
+	VariableTypes type;
+	void* data;
+};
+
+
 
 class Lexer
 {
 public:
 	Lexer();
 	~Lexer();
+	void FirstPass();
+	void ReadFile(char *filePath);
 
-	void OpenFile(char *filePath);
+	void TokenizeLine(string &str);
+	void DoLine(vector<string> &line);
+	void ChangeFunction(unsigned vectorIndex);
+	void GetMatchingParens(unsigned leftParens, vector<string> &tokens);
+	void SetupFunction(unsigned leftParens, vector<string> &tokens);
+	
 protected:
-
+	int currentLine;
 	vector<string> callStack;
+	vector<Function> vectorOfFunctions;
 	vector<string> fileLines;
+	vector<Variable> variables;
 };
--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@ int main(int argc, char **argv)
 {
 	for(unsigned i = 0; i < argc; i++)
 	{
-		
+		Lexer interpreter; 
+		interpreter.FirstPass(argv[i]);
 	}
 	return 0;
 }
