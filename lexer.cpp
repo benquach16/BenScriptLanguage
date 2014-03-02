07c9c36b98b7446863b1a7fba7304e84df8da470
thesultanster <khansultan024@gmail.com>
2014-03-02 06:56:35 -0800

    Added perliminary email functionality

--- lexer.cpp
+++ lexer.cpp
@@ -1594,3 +1594,23 @@ int Lexer::FindPCBack(bool elseOk)
 			return i;
 	}
 }
+
+int sendmail(const char *to, const char *from, const char *subject, const char *message)
+{
+	int retval = -1;
+	FILE *mailpipe = popen("/usr/lib/sendmail -t", "w");
+	if (mailpipe != NULL) 
+	{
+		fprintf(mailpipe, "To: %s\n", to);
+		fprintf(mailpipe, "From: %s\n", from);
+		fprintf(mailpipe, "Subject: %s\n\n", subject);
+		fwrite(message, 1, strlen(message), mailpipe);
+		fwrite(".\n", 1, 2, mailpipe);
+		pclose(mailpipe);
+		retval = 0;
+	}
+	else 
+		perror("Failed to invoke sendmail");
+	
+	return retval;
+}
